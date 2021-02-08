0de4cec7e4d76df79d6a0e712562fb6e1a2d3d7a
James Pedersen <james00090@gmail.com>
2021-02-08 00:23:29 -0800

    removed providers spec from main.tf

--- gcp-kubeadm/main.tf
+++ gcp-kubeadm/main.tf
@@ -1,22 +1,3 @@
-terraform {
-  required_providers {
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = ">= 1.11"
-    }
-    null = {
-      source  = "hashicorp/null"
-      version = ">= 2.1"
-    }
-    google = {
-      source  = "hashicorp/google"
-      version = "3.53.0"
-    }
-  }
-  required_version = ">= 0.13"
-}
-
-
 module "k8s" {
   source = "./modules/k8s"
 
