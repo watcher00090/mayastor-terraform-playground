0f396bccf6c0983c48c03248dd8bd7352fb1964d
James Pedersen <james00090@gmail.com>
2021-02-08 17:57:27 -0800

    removed test.bat

--- gcp-kubeadm/modules/k8s/scripts/test.bat
+++
@@ -1,1 +0,0 @@
-ssh -i C:/Users/pcp071098/Documents/mayastor-terraform-gcp.pem -o UserKnownHostsFile=%USERPROFILE%/.ssh/known_hosts -o StrictHostKeyChecking=no -t ubuntu-user@%INSTANCE_IPV4_ADDRESS% sudo vim /etc/ssh/sshd_config -c \^"%s/PermitRootLogin\s\+\w\+/PermitRootLogin yes/gi ^| wq\^"
