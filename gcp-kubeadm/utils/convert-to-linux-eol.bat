ab736357b1ca5bda4b86f755a5ad17688e5ec91c
watcher00090 <james00090@gmail.com>
2021-02-10 18:39:09 -0800

    removed utils/ directory

--- gcp-kubeadm/utils/convert-to-linux-eol.bat
+++
@@ -1,8 +0,0 @@
-
-set "COMMAND_STR=%~dp0\conv-eol-in-place.bat @file %~dp0\..\%FILES_DIR%"
-
-if EXIST %FILES_DIR% (
-    forfiles /p %FILES_DIR% /c "cmd /c %COMMAND_STR%"
-) ELSE (
-    echo "convert-to-linux-eol.bat: no files to convert, returning...."
-)
