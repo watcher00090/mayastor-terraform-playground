b31321b3a5bc4fc3fbb83cc49e9971d34a7fb8f0
= <james00090@gmail.com>
2021-02-11 16:15:39 -0800

    small changes

--- gcp-kubeadm/modules/k8s/main.tf
+++ gcp-kubeadm/modules/k8s/main.tf
@@ -190,7 +190,7 @@ sudo systemctl restart ssh
       install_packages   = var.install_packages,
       kubernetes_version = var.kubernetes_version,
       server_upload_dir  = var.server_upload_dir,
-      node_name = "worker-${count.index}"
+      node_name = "worker-${count.index+1}"
     })
     destination = "${var.server_upload_dir}/bootstrap.sh"
   }
