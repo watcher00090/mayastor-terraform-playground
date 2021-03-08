481ee98a4f6c57f0eb5506da7b05e6dcb7134fc0
= <james00090@gmail.com>
2021-03-07 16:34:00 -0800

    using absolute path for kubeconfig file in module outputs

--- gcp-kubeadm/outputs.tf
+++ gcp-kubeadm/outputs.tf
@@ -1,21 +1,5 @@
-#output "master_ip" {
-#  value = module.k8s.master_ip
-#}
-
-#output "node_ips" {
-#  value = [for worker in slice(module.k8s.cluster_nodes, 1, length(module.k8s.cluster_nodes)) : worker.public_ip]
-#}
-
-#output "k8s_admin_conf" {
-#  value = abspath(module.k8s.k8s_admin_conf)
-#}
-
-#output "kubeconfig" {
-#  value = abspath(module.k8s.k8s_admin_conf)
-#}
-
 output "kubeconfig" {
-  value       = module.k8s.kubeconfig
+  value       = abspath(pathexpand(module.k8s.kubeconfig))
   description = "Location of the kubeconfig file for the created cluster on the local machine."
 }
 
